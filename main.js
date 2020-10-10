0a0294d6f2e0f2e0cf5c7493953cebea8e40af01
Harpreet Goraya <harpreetgoraya95@gmail.com>
2020-10-09 23:44:35 -0400

    added auth

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 const axios = require('axios').default;
 
+const TOKEN = '8cca1eba25725a58eb512fb659d5c75e6c3c8859'
+const TOKEN2 = '35d4fec32f12a8c23805622dd6ce61825c9f1e96'
+
 async function getUserData(locationn){
     const URL = `https://api.github.com/search/users?q=location:${locationn}&per_page=10`
 
     try {
-        const data = (await axios.get(URL)).data;
-        console.log(response);
+        const data = (await axios.get(URL,{
+            headers:{
+                'Authorization' : `token ${TOKEN}`
+            }
+        })).data;
+
+        console.log(data);
         
     } catch (error) {
-        console.log(error);
+        console.log("Axios error");
     }
 }
 
